4a36744318cfd52b8b70e2ad9a6e61237dc21399
srija65 <61839351+srija65@users.noreply.github.com>
2020-07-21 23:04:11 +0530

    Update tripletsum.c

--- 21.07/tripletsum.c
+++ 21.07/tripletsum.c
@@ -17,12 +17,12 @@ int main()
    
    for(i=0;i<(n-2);i++)
    {
-      for (int j=i+1;i<(n-1);j++) { 
+      for (int j=i+1;j<(n-1);j++) { 
         for (int k=(j+1);k<n; k++) { 
             if (a[i]+a[j]+a[k]==sum) { 
                 printf("Triplets are\n");
                 printf("%d %d %d",a[i],a[j],a[k]);
-                return 1;
+                
             }
         }
       }
