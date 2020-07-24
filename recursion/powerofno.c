47d27438eb6456675d75501ca6aca2eedc8063cb
srija65 <61839351+srija65@users.noreply.github.com>
2020-07-24 11:58:51 +0530

    Update powerofno.c

--- recursion/powerofno.c
+++ recursion/powerofno.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
-int sum(int n);
+int power(int a,int b);
 int main()
 {
-    int n;
-    printf("Enter the no:\n");
-    scanf("%d",&n);
-    printf("Sum of the nos:%d",sum(n));
+    int a,b;
+    printf("enter base\n");
+    scanf("%d",&a);
+    printf("enter exponent");
+    scanf("%d",&b);
+    printf("the answer is %d",power(a,b));
+    
 
     return 0;
 }
-int sum(int n)
-{
-    if(n==1){
+int power(int a,int b){
+    if(b==0)
+    {
         return 1;
     }
-    return n+sum(n-1);
+    else if(b%2==0)
+    {
+        return power(a,b/2)*power(a,b/2);
+        
+    }
+    else
+    {
+        return a*power(a,b/2)*power(a,b/2);
+    }
 }
 
