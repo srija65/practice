50c4d3d2f0d9591e6f92637de498af03eb38c148
srija65 <61839351+srija65@users.noreply.github.com>
2020-07-24 19:49:18 +0530

    Update recursive.c

--- 24.7/binary search/recursive.c
+++ 24.7/binary search/recursive.c
@@ -8,13 +8,13 @@ int Recbs(int A[],int start,int end,int ele) {
     if( A[mid]==ele) 
         return mid;
     else if(ele<A[mid])
-        Recbs(A,start,mid-1,ele);
+        return Recbs(A,start,mid-1,ele);
     else
-        Recbs(A,mid+1,end,ele);
+        return Recbs(A,mid+1,end,ele);
 }
 
 int main() {
-  /*int a[1000],x,n;
+  int a[1000],x,n;
    printf("Enter size:\n");
    scanf("%d",&n);
    printf("Enter element into array:\n");
@@ -22,12 +22,13 @@ int main() {
   {
        scanf("%d",&a[i]);
        
-   }*/
-   int x;
-   int A[] = {0,12,6,12,12,18,34,45,55,99};
+   }
+   /*int x;
+   int A[] = {0,12,6,12,12,18,34,45,55,99};*/
    printf("enter element to be search:\n");
    scanf("%d",&x);
-   printf("%d is found at Index %d \n",x,Recbs(A,0,9,x));
+   printf("%d is found at Index %d \n",x,Recbs(a,0,n,x));
    return 0;
 }
+
 
